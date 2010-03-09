8c7f350867b0b7f840e8d13546ffc0075a6d9dfc
Phoebe <fireant@comcast.net>
2010-03-09 10:55:41 -0500

    same as last

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,11 @@
 class Tag < ActiveRecord::Base
   belongs_to :creator, :class_name=> "User" #, :foreign_key => :creator_id
-  has_many :children, :class_name => "Tag", :foreign_key=>:parent_id
   belongs_to :parent, :class_name=>"Tag"
+  has_many :children, :class_name => "Tag", :foreign_key=>:parent_id
+  has_many :profile_tags
   has_many :profiles, :through => :profiles_tags
+
+  validates_uniqueness_of :name
+  validates_uniqueness_of :uri
+
 end
