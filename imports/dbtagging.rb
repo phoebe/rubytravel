df602ab5a97446cfa2df34150e5b613845e6cd66
Phoebe <fireant@comcast.net>
2010-05-04 16:26:56 -0400

    delete

--- imports/dbtagging.rb
+++
@@ -1,4 +0,0 @@
-select distinct * where {?a ?b category:Snow_tubing_areas_in_California  }
-Category:Snow_tubing_areas_in_the_United_States
-select distinct * where { ?c <http://www.w3.org/2004/02/skos/core#broader> category:Snow_tubing_areas_in_the_United_States . ?a <http://www.w3.org/2004/02/skos/core#subject> ?c}
-select distinct * where { _:c <http://www.w3.org/2004/02/skos/core#broader> category:Snow_tubing_areas_in_the_United_States . ?a <http://www.w3.org/2004/02/skos/core#subject> _:c}
--- imports/test/testsparql.rb
+++
@@ -1,6 +0,0 @@
-
-require 'rubygems'
-require 'sparql/client'
-
-sparql = SPARQL::Client.new("http://dbpedia.org/sparql")
-
