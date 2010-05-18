6c5f58837ca1790c58ab93d7fa0ddfa6e7a42fde
Phoebe <fireant@comcast.net>
2010-05-18 14:50:50 -0400

    debug mesg

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -1,10 +1,12 @@
 module TripsHelper
   def getTags 
     #@participations=@trip.participations
+    unless @participations.nil?
     profile_list= @participations.profile_id.collect
     @tags= Profile_Tags.find(
       :all,
       :conditions =>{ :profile_id_in => profile_list }
     )
+    end
   end
 end
