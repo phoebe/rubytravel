ce81619955c00c12960bacd55e47db7d35d1f77a
Ron Newman <rnewman@thecia.net>
2010-07-21 23:40:16 -0400

    remove 'login' field from test/fixtuers/users.yml and test/functional/clearance/users_controller_test.rb (otherwise, tests fail!)

--- test/functional/clearance/users_controller_test.rb
+++ test/functional/clearance/users_controller_test.rb
@@ -4,7 +4,7 @@ class Clearance::UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, :user => { :login=>'josie',:email=>'josie@gmail.com',:password=>'josie', :password_confirmation=>'josie', :email_confirmed => true, :latitude => 42.383, :longitude=>-71.055}
+      post :create, :user => {:email=>'josie@gmail.com',:password=>'josie', :password_confirmation=>'josie', :email_confirmed => true, :latitude => 42.383, :longitude=>-71.055}
     end
 
     assert_redirected_to sign_in_path
